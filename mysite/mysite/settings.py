319f2d6569fb856b222c13ac3c8aac9e4791768b
Alvaro Perez <alvaro@MacBook-Pro-de-Alvaro.local>
2019-05-24 02:33:09 +0200

    cambiada url para que devuelva a localhost

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -87,7 +87,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
 )
 
-BASEURL = 'https://front3-datame.herokuapp.com'
+BASEURL = 'http://localhost:8080'
 
 ROOT_URLCONF = 'mysite.urls'
 
@@ -190,7 +190,7 @@ PAYPAL_MODE= "sandbox"
 PAYPAL_CLIENT_ID = "AQnKuEyaCgxZsEBPC-aq3glrpVK1dcTUUbvjkmnguZOM1WLQnsEU-y7i2oh3VkM_Xm4AfbJQx8AKdiYF"
 PAYPAL_CLIENT_SECRET = "EEkGJcVWziLr8FnQFC-BCYX0bcSz_xyh3POT7iR1K9e7fxFLVhI9EJSnOl5Jn3d_UhqydqGEYTfzCozc"
 
-SITE_URL ='https://front3-datame.herokuapp.com/'
+SITE_URL ='http://localhost:8080/'
 
 import django_heroku
 django_heroku.settings(locals())
